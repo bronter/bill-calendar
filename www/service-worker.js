f1b04cba31216ce074a138535eb916f3089715a2
Gabriel Charleboix <gcharleboix@gmail.com>
2023-02-08 23:35:03 -0600

    basic PWA stuff

---
+++ www/service-worker.js
@@ -0,0 +1,47 @@
+// reference: https://paweldymek.com/en/post/pwa-minimum-requirements
+
+const CACHE_NAME = 'bill-calendar-0.0.0';
+const URLS_TO_CACHE = [
+    '/index.html',
+    '/index.css',
+    '/calendar-day.js',
+    '/calendar.js'
+];
+
+self.addEventListener('install', function(event) {
+    const cachePromise = caches.open(CACHE_NAME);
+    cachePromise.then(cache => cache.addAll(URLS_TO_CACHE));
+    event.waitUntil(cachePromise);
+});
+
+self.addEventListener('activate', event => {
+    const cachePromise = caches
+        .keys()
+        .then(keylist => Promise.all(keyList.map(key => {
+            if (key !== CACHE_NAME) {
+                return caches.delete(key);
+            }
+        })));
+    event.waitUntil(cachePromise);
+})
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    if (request.url.startsWith(self.location.origin)) {
+        const cachePromise = caches.match(request)
+            .then(cache => {
+                if (cache) {
+                    return cache;
+                }
+    
+                return caches.open(CACHE_NAME).then(cache => {
+                    if (!navigator.onLine) {
+                        return;
+                    }
+
+                    return fetch(event.request);
+                });
+            });
+        event.respondWith(cachePromise);
+    }
+});
