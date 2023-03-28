757d8a6c2fbf3e238bb2f7d5a215ecd38f71b457
Gabriel Charleboix <gcharleboix@gmail.com>
2023-03-28 09:42:59 -0500

    Peg totals table to the browser time on page load (will make it update later)

--- www/ui-main.js
+++ www/ui-main.js
@@ -14,14 +14,22 @@ const totalDueElement = document.getElementById('total-due');
 const totalPastDueElement = document.getElementById('total-past-due');
 const totalPaidElement = document.getElementById('total-paid');
 
-let currentDate;
+let selectedDate;
 let selectedDayOfMonth; // Not zero-based
-let currentYear;
-let currentMonth;
-let currentDateString;
+let selectedYear;
+let selectedMonth;
+let selectedDateString;
 let daysInMonth;
 let startDay;
 
+let now = new Date();
+let currentYear = now.getFullYear();
+let currentMonth = now.getMonth();
+let currentDay = now.getDate();
+// Omit the specific time and have it be 0:00 local time
+let currentDate = new Date(currentYear, currentMonth, currentDay);
+
+let billsSelectedMonth = [];
 let billsThisMonth = [];
 
 let totalDue = 0;
@@ -29,25 +37,26 @@ let totalPastDue = 0;
 let totalPaid = 0;
 
 function afterDateUpdateData(newDate, dateString) {
-    currentDate = newDate;
-    currentDateString = dateString;
-    selectedDayOfMonth = currentDate.getDate();
-    currentYear = currentDate.getFullYear();
-    currentMonth = currentDate.getMonth();
-    daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-    startDay = days[new Date(currentYear, currentMonth, 1).getDay()];
+    selectedDate = newDate;
+    selectedDateString = dateString;
+    selectedDayOfMonth = selectedDate.getDate();
+    selectedYear = selectedDate.getFullYear();
+    selectedMonth = selectedDate.getMonth();
+    daysInMonth = new Date(selectedYear, selectedMonth + 1, 0).getDate();
+    startDay = days[new Date(selectedYear, selectedMonth, 1).getDay()];
 }
 afterDateUpdateData(new Date());
 
-const formattedYear = String(currentYear).padStart(4, '0');
-const formattedMonth = String(currentMonth + 1).padStart(2, '0');
+const formattedYear = String(selectedYear).padStart(4, '0');
+const formattedMonth = String(selectedMonth + 1).padStart(2, '0');
 const formattedDay = String(selectedDayOfMonth).padStart(2, '0');
-currentDateString = `${formattedYear}-${formattedMonth}-${formattedDay}`;
+selectedDateString = `${formattedYear}-${formattedMonth}-${formattedDay}`;
 
 function updateTotals(bills) {
     totalDue = 0;
     totalPastDue = 0;
     totalPaid = 0;
+    const currentTime = currentDate.getTime();
 
     for (const [index, dayBills] of bills.entries()) {
         const date = new Date(currentYear, currentMonth, index + 1);
@@ -57,7 +66,9 @@ function updateTotals(bills) {
                 totalPaid += bill.amount;
             } else {
                 totalDue += bill.amount;
-                if (date.getTime() < currentDate.getTime()) {
+                // We use the browser's selected time rather than the selected date;
+                // we never want to give the user a false impression that bills are overdue
+                if (date.getTime() < currentTime) {
                     totalPastDue += bill.amount;
                 }
             }
@@ -78,12 +89,13 @@ const calendarElement = document.getElementById('calendar');
 
 // Stuff that is updated every time including the first time
 function updateHeadersAndCalendar() {
-    dateNav.value = currentDateString;
-    monthLabel.textContent = months[currentMonth];
+    dateNav.value = selectedDateString;
+    monthLabel.textContent = months[selectedMonth];
     calendarElement.setAttribute('days-in-month', daysInMonth);
     calendarElement.setAttribute('start-day', startDay);
+    billsSelectedMonth = billsForMonth(selectedMonth, selectedYear);
     billsThisMonth = billsForMonth(currentMonth, currentYear);
-    calendarElement.setBills(billsThisMonth);
+    calendarElement.setBills(billsSelectedMonth);
 
     updateTotals(billsThisMonth);
     updateTotalsTable();
@@ -96,7 +108,7 @@ dateNav.addEventListener('change', e => {
     const dateStr = e.target.value;
 
     const newSelectedDayOfMonth = parseInt(dateStr.substring(yyyymmLength + 1), 10);
-    if (dateStr.substring(0, yyyymmLength) !== currentDateString.substring(0, yyyymmLength)) {
+    if (dateStr.substring(0, yyyymmLength) !== selectedDateString.substring(0, yyyymmLength)) {
         // Giving the Date constructor a time with no timezone makes it use the local timezone
         // It shouldn't matter what the time is since we don't use that info, so I set it to midnight.
         const newDate = new Date(`${dateStr}T00:00:00`);
@@ -110,19 +122,19 @@ dateNav.addEventListener('change', e => {
 });
 
 export function dateForDay(dayOfMonth) {
-    return new Date(currentYear, currentMonth, dayOfMonth);
+    return new Date(selectedYear, selectedMonth, dayOfMonth);
 }
 
 export function billsForDay(dayOfMonth) {
-    return billsThisMonth[dayOfMonth - 1];
+    return billsSelectedMonth[dayOfMonth - 1];
 }
 
 addBillDialog.addEventListener('submit', e => {
     const {amount, name, startDate, endDate, type} = e.detail;
     const bill = newBill(parseInt(amount, 10), name, startDate, endDate, type);
     const day = startDate.getDate();
-    billsThisMonth[day - 1].push(bill);
-    updateTotals(billsThisMonth);
+    billsSelectedMonth[day - 1].push(bill);
+    updateTotals(billsSelectedMonth);
     updateTotalsTable();
     calendarElement.addBillToDay(day, bill);
 });
